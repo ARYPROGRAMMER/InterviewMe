534c355f84bdc32f5fd927ab0258af9de7a955e8
Arya Pratap Singh <notaryasingh@gmail.com>
2025-04-12 17:56:03 +0530

    psudeo middleware
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,35 +1,34 @@
 "use server";
 
-import { db , auth} from "@/firebase/admin";
+import { db, auth } from "@/firebase/admin";
 import { cookies } from "next/headers";
 
-const ONE_WEEK =  60*60*24*7*1000;
+const ONE_WEEK = 60 * 60 * 24 * 7 * 1000;
 
 export async function signUp(params: SignUpParams) {
   const { uid, name, email } = params;
 
   try {
-
-    const userRecord = await db.collection('users').doc(uid).get()
+    const userRecord = await db.collection("users").doc(uid).get();
 
     if (userRecord.exists) {
-        return {
-            success: false,
-            message: "User already exists. Please sign in instead"
-        }
+      return {
+        success: false,
+        message: "User already exists. Please sign in instead",
+      };
     }
 
-    await db.collection('users').doc(uid).set({
-        name,email
-    })
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+    });
 
     return {
       success: true,
-      message : "Account created successfully. Please sign in"
-    }
+      message: "Account created successfully. Please sign in",
+    };
 
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (e: any) {
     console.error("Error creating a user", e);
 
@@ -41,51 +40,77 @@ export async function signUp(params: SignUpParams) {
     }
 
     return {
-        success: false,
-        message: "Failed to create an account"
-    }
-
+      success: false,
+      message: "Failed to create an account",
+    };
   }
 }
 
-export async function signIn(params : SignInParams) {
-  const {email , idToken} = params;
+export async function signIn(params: SignInParams) {
+  const { email, idToken } = params;
 
   try {
-      const userRecord = await auth.getUserByEmail(email);
-      if (!userRecord){
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord) {
+      return {
+        success: false,
+        messgae: "User does not exist. Create an account now",
+      };
+    }
 
-        return {
-          success: false,
-          messgae: "User does not exist. Create an account now"
-        }
-      }
-
-      await setSessionCookie(idToken);
+    await setSessionCookie(idToken);
   }
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  catch (e: any){
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any 
+  catch (e: any) {
 
     console.log(e);
 
     return {
-      success : false,
-      message : "Failed to log into the account"
-    }
+      success: false,
+      message: "Failed to log into the account",
+    };
   }
 }
 
-export async function setSessionCookie(idToken : string) {
+export async function setSessionCookie(idToken: string) {
   const cookieStore = await cookies();
   const sessionCookie = await auth.createSessionCookie(idToken, {
-    expiresIn: ONE_WEEK
-  })
+    expiresIn: ONE_WEEK,
+  });
 
-  cookieStore.set('session',sessionCookie,{
+  cookieStore.set("session", sessionCookie, {
     maxAge: ONE_WEEK,
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    path: '/',
-    sameSite: 'lax'
-  })
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("session")?.value;
+
+  if (!sessionCookie) return null;
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+    const userRecord = await db.collection('users').doc(decodedClaims.uid).get();
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User
+  }
+  //eslint-disable-next-line @typescript-eslint/no-explicit-any 
+  catch (e: any) {
+    console.log(e);
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+
+  return !!user;
 }
